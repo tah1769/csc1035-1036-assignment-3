2bf978349d79aa295160920f45ab1b2fe992be87
Tah749 <60526586+Tah749@users.noreply.github.com>
2024-04-29 15:04:21 +0100

    expenses ArrayList properly created

--- src/assignment3/packages/ExpensesManager.java
+++ src/assignment3/packages/ExpensesManager.java
@@ -10,7 +10,7 @@ public class ExpensesManager {
 
     public ExpensesManager() {
         // initially no expense
-        expenses = new ArrayList<>;
+        expenses = new ArrayList<>();
     }
 
     // add an expense to the existing list of expenses
