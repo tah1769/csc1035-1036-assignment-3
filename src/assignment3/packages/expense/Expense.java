3ed7ab779123a443fa35d701c50b51a7d2c0b25b
Tah749 <60526586+Tah749@users.noreply.github.com>
2024-05-07 19:15:21 +0100

    Currency attribute added

--- src/assignment3/packages/expense/Expense.java
+++ src/assignment3/packages/expense/Expense.java
@@ -1,8 +1,9 @@
 package assignment3.packages.expense;
 
 import assignment3.packages.category.Category;
+import assignment3.packages.currency.Currency;
 
 import java.io.Serializable;
 import java.time.LocalDate;
 
-public record Expense(double amount, Category category, LocalDate date) implements Serializable { }
+public record Expense(double amount, Category category, Currency currency, LocalDate date) implements Serializable { }
